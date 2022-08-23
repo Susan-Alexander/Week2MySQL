7376ba56e9c4ca5230112ee4028fbfd340346a6d
Susan-Alexander <susan.alexander.artist@gmail.com>
2022-08-22 21:29:55 -0700

    First Commit

---
+++ src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,17 @@
+package projects;
+
+import java.sql.Connection;
+
+import projects.dao.DbConnection;
+
+//@SuppressWarnings("unused")
+@SuppressWarnings("unused")
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DbConnection.getConnection();
+
+	}
+
+}
